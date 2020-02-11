72cd52a5d993ff45c27baa2f4aecba5f8bba56e6
Ilyasklyar <sklyarov.illya@gmail.com>
2020-02-11 02:10:43 +0200

    stopSubmit redux-form

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,7 +5,7 @@ import { required } from '../utils/validators/validators';
 import { connect } from 'react-redux';
 import {login,logout} from "../Redux/auth-reducer" 
 import { Redirect } from 'react-router-dom';
-
+import style from '../components/common/FormsControls/FormsControls.module.css'
 
 const LoginForm = (props) => {
     return (
@@ -21,6 +21,11 @@ const LoginForm = (props) => {
             <div>
                 <Field component={Input} name={'rememberMe'} type={'checkbox'} />remember me
             </div>
+           { props.error &&
+            <div className={style.formSummaryError}>
+                {props.error}
+            </div>
+            }
             <div>
                 <button>Login</button>
             </div>
